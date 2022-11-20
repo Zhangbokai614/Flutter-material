9a9e485d8923a86189d66a0585e403ceaceacf5f
Zhangbokai614 <614525683@qq.com>
2022-11-21 02:20:36 +0800

    update: page layout encapsulation

--- lib/pages/asset_management/index.dart
+++ lib/pages/asset_management/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/layout/head_row_three_parts.dart';
 import '_top.dart';
 import '_left.dart';
 import '_middle.dart';
@@ -14,58 +15,35 @@ class AssetManagementPage extends StatelessWidget {
       data: ThemeData(
         colorScheme: const ColorScheme.light().copyWith(
           primary: Colors.white,
-          secondary: const Color(0x001b1b1b),
-          onSecondary: const Color(0x00181818),
-          tertiary: const Color(0x00ff2424),
-          onTertiary: const Color(0x0054b992),
-          background: const Color(0x00f7f7f7),
+          secondary: const Color(0xff1b1b1b),
+          onSecondary: const Color(0xff181818),
+          tertiary: const Color(0xffff2424),
+          onTertiary: const Color(0xff54b992),
+          background: const Color(0xfff7f7f7),
         ),
         textTheme: const TextTheme(
           headline3: TextStyle(
-            color: Color(0x00101828),
+            color: Color(0xff101828),
           ),
           headline5: TextStyle(
-            color: Color(0x00101828),
+            color: Color(0xff101828),
           ),
-          bodyText1: TextStyle(
-            color: Color(0x00e6e6e6),
+          headline6: TextStyle(
+            color: Color(0xffe6e6e6),
           ),
-          bodyText2: TextStyle(
-            color: Color(0x009b9b9b),
+          subtitle1: TextStyle(
+            color: Color(0xff9b9b9b),
           ),
           subtitle2: TextStyle(
-            color: Color(0x005b606b),
+            color: Color(0xff5b606b),
           ),
         ),
       ),
-      child: Flex(
-        direction: Axis.vertical,
-        children: [
-          const Expanded(
-            flex: 86,
-            child: PageTop(),
-          ),
-          Expanded(
-            flex: 914,
-            child: Flex(
-              direction: Axis.horizontal,
-              children: const [
-                Expanded(
-                  flex: 184,
-                  child: PageLeft(),
-                ),
-                Expanded(
-                  flex: 552,
-                  child: PageMiddle(),
-                ),
-                Expanded(
-                  flex: 264,
-                  child: PageRight(),
-                ),
-              ],
-            ),
-          )
-        ],
+      child: HeadRowThreeParts(
+        top: PageTop(),
+        left: PageLeft(),
+        middle: PageMiddle(),
+        right: PageRight(),
       ),
     );
   }
---
+++ lib/widgets/layout/head_row_three_parts.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class HeadRowThreeParts extends StatelessWidget {
+  const HeadRowThreeParts({
+    super.key,
+    this.flex = const [86, 914, 184, 552, 264],
+    this.top,
+    this.left,
+    this.middle,
+    this.right,
+  });
+
+  final List<int> flex;
+  final Widget? top;
+  final Widget? left;
+  final Widget? middle;
+  final Widget? right;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Expanded(
+          flex: flex[0],
+          child: _ShadowBox(
+            child: top,
+          ),
+        ),
+        Expanded(
+          flex: flex[1],
+          child: Flex(
+            direction: Axis.horizontal,
+            children: [
+              Expanded(
+                flex: flex[2],
+                child: _ShadowBox(
+                  child: left,
+                ),
+              ),
+              Expanded(
+                flex: flex[3],
+                child: _ShadowBox(
+                  child: middle ??
+                      Flexible(
+                        child: Container(
+                            color: Theme.of(context).colorScheme.background),
+                      ),
+                ),
+              ),
+              Expanded(
+                flex: flex[4],
+                child: _ShadowBox(
+                  child: right,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _ShadowBox extends StatelessWidget {
+  const _ShadowBox({this.child});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        boxShadow: const [
+          BoxShadow(
+            offset: Offset(2, 1),
+            blurStyle: BlurStyle.normal,
+            blurRadius: 2,
+            color: Colors.black38,
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
